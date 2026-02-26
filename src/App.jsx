980d05a445445732069e9693598cdd70540557b9
Samia123-source <samiamahim04@gmail.com>
2026-02-26 15:29:10 +0600

    state,useEffect

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import Posts from './Posts'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>     
+      <h3>React Core Concepts</h3>
+      <ol>
+        <li>Components</li>
+        <li>JSX</li>
+        <li>Props</li>
+        <li>Eventhandler</li>
+        <li>State</li>
+        <li>load Data</li>
+      </ol>
+      <hr />
+      <Posts></Posts>
+      
+    </>
+  )
+}
+
+export default App
